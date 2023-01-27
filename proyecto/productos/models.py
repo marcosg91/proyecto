23b78f6f561835d3ae77d63a34592531987d303b
unknown <marcos_g91@hotmail.com>
2023-01-27 11:19:10 -0300

    creamos aplicaicon para producto

--- proyecto/productos/models.py
+++ proyecto/productos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    nombre=models.CharField(max_length=255)
+    precio=models.DecimalField(max_digits=6, decimal_places=2)
+    descripcion=models.TextField()
+
+    def __str__(self):
+        return self.nombre
--- proyecto/proyecto/views.py
+++ proyecto/proyecto/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render 
+from productos.models import Producto
 
 def inicio(request):
     template_name = "index.html"
+    """"
+    p=Producto.objects.create(
+        nombre="pantalon",
+        precio=1500,
+        descripcion="pantalaon azul"
+    )
+    """
+    productos=Producto.objects.all()
+    
     contexto = {
-
+        'productos': productos
     }
     return render(request, template_name, contexto)
