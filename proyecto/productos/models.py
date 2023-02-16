2f32ff525d6a2a8455fd634d63ebaf1850585242
unknown <marcos_g91@hotmail.com>
2023-02-16 20:00:07 -0300

    image field

--- proyecto/productos/models.py
+++ proyecto/productos/models.py
@@ -9,23 +9,20 @@ class Producto(models.Model):
 
     activo=models.BooleanField(default=True)
 
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True, related_name="categorias")
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True, related_name="categorias", blank=True)
 
     categoria2 = models.ManyToManyField(Categoria)
 
-
     #ficha = models.OneToOneField(Ficha)  para los casos de 1 a 1 (a un producto le corresponde una ficha); se deberia crear el modelo Ficha
 
+    imagen = models.ImageField(upload_to="productos", null=True, blank=True)
+    #tambien existe archivo = models.FileField
 
     def __str__(self):
         return self.nombre
 
 
-
-
-
 """
-
 c = Categoria.objects.get(id=1)
 c.categorias.all()
 
--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from django.contrib.auth import views as auth_views 
 
@@ -14,5 +16,5 @@ urlpatterns = [
      #includes
     path('productos/', include('productos.urls')),
     path('usuarios/', include('usuarios.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
    
